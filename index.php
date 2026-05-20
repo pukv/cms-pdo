3bbd6ec02100692bce181650e845acf11f6b8b84
pukv <pukvavlovic10@gmail.com>
2026-05-21 00:20:22 +0200

    instantiatied the DB, created a new connection using the getConnection fn

--- index.php
+++ index.php
@@ -2,6 +2,14 @@
 include "partials/header.php";
 include "partials/navbar.php";
 include "partials/hero.php";
+
+$db = new Database();
+
+// echo "<pre>";
+// var_dump($db);
+// echo "<pre>";
+
+$db->getConnection();
 ?>
 
     <!-- Main Content -->
@@ -18,7 +26,7 @@ include "partials/hero.php";
             <div class="col-md-8">
                 <h2>Blog Post Title 1</h2>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros, 
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque nisl eros,
                     pulvinar facilisis justo mollis, auctor consequat urna.
                 </p>
                 <a href="article.html" class="btn btn-primary">Read More</a>
@@ -36,7 +44,7 @@ include "partials/hero.php";
             <div class="col-md-8">
                 <h2>Blog Post Title 2</h2>
                 <p>
-                    Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in, 
+                    Morbi in sem quis dui placerat ornare. Pellentesque odio nisi, euismod in,
                     pharetra a, ultricies in, diam. Sed arcu.
                 </p>
                 <a href="#" class="btn btn-primary">Read More</a>
@@ -54,7 +62,7 @@ include "partials/hero.php";
             <div class="col-md-8">
                 <h2>Blog Post Title 3</h2>
                 <p>
-                    Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu 
+                    Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu
                     vulputate magna eros eu erat.
                 </p>
                 <a href="#" class="btn btn-primary">Read More</a>
@@ -62,6 +70,5 @@ include "partials/hero.php";
         </div>
     </main
 
-<?php
-include "partials/footer.php";
+<?php include "partials/footer.php";
 ?>
